52e799e4a7bf88233eb86407e2c20c7a7a60781b
SarthakJoshi21 <157214236+SarthakJoshi21@users.noreply.github.com>
2024-10-19 13:33:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name; // Use private property for name
+    this.age = age; // Public property for age with setter
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(age) {
+    if (typeof age !== 'number' || age < 0) {
+      throw new Error('Age must be a positive number');
+    }
+    this._age = age;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age); // Call parent constructor
+  }
+
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age); // Call parent constructor
+  }
+
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
-window.Teacher = Teacher;
+window.Teacher = Teacher;   
